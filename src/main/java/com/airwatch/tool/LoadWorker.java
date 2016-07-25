7cac92820821860c16386ebc33660f9b6703a1bf
manishk <Water@1234>
2016-07-24 21:52:58 -0400

    Allow the capability to define the percentage break between requests type/command.

--- src/main/java/com/airwatch/tool/LoadWorker.java
+++ src/main/java/com/airwatch/tool/LoadWorker.java
@@ -161,7 +161,6 @@ public class LoadWorker extends AbstractVerticle {
     }
 
     private enum CommandType {
-
         SYNC("/Microsoft-Server-ActiveSync?Cmd=Sync&"),
         PING("/Microsoft-Server-ActiveSync?Cmd=Ping&"),
         ITEM_OPERATIONS("/Microsoft-Server-ActiveSync?Cmd=ItemOperations&");
